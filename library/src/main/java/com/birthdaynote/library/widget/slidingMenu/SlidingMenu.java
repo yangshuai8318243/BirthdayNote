1308f87b8d8f45990123abebf308dfb2d8f26f49
yangshuai <519775704@qq.com>
2020-08-23 11:13:34 +0800

    update：侧滑例子

---
+++ library/src/main/java/com/birthdaynote/library/widget/slidingMenu/SlidingMenu.java
@@ -0,0 +1,72 @@
+package com.birthdaynote.library.widget.slidingMenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+/**
+ * https://blog.csdn.net/lmj623565791/article/details/39185641
+ */
+public class SlidingMenu extends HorizontalScrollView {
+    /**
+     * 屏幕宽度
+     */
+    private int mScreenWidth;
+    /**
+     * dp
+     */
+    private int mMenuRightPadding = 50;
+    /**
+     * 菜单的宽度
+     */
+    private int mMenuWidth;
+    private int mHalfMenuWidth;
+
+    private boolean once;
+
+    public SlidingMenu(Context context) {
+        super(context);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        /**
+         * 显示的设置一个宽度
+         */
+        if (!once) {
+            LinearLayout wrapper = (LinearLayout) getChildAt(0);
+            ViewGroup menu = (ViewGroup) wrapper.getChildAt(0);
+            ViewGroup content = (ViewGroup) wrapper.getChildAt(1);
+            // dp to px
+            mMenuRightPadding = (int) TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP, mMenuRightPadding, content
+                            .getResources().getDisplayMetrics());
+
+            mMenuWidth = mScreenWidth - mMenuRightPadding;
+            mHalfMenuWidth = mMenuWidth / 2;
+            menu.getLayoutParams().width = mMenuWidth;
+            content.getLayoutParams().width = mScreenWidth;
+
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
